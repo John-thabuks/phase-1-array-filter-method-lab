bb7eccd20fa6c690e63a3c7566b8ed88757e6002
John-thabuks <thabuksjohn@gmail.com>
2023-10-08 11:50:35 +0300

    Assignment Completed.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+   return drivers.filter(element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(driver, firstLetter){
+   return driver.filter(letter => letter.toLowerCase().startsWith(firstLetter.toLowerCase()))
+}
+
+function  matchName(drivers, driverName){
+   return drivers.filter(search => search.name === driverName );
+}
